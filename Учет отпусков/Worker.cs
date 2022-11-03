70a05f799744a20779421b1575c178654b24a672
sleeme2020@yandex.ru <sleeme2020@yandex.ru>
2022-11-03 15:51:47 +0300

    Накинул модельку подготовил описание, нужно описать VM и код модели

---
+++ Учет отпусков/Worker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Учет_отпусков
+{
+    public class Worker
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public DateOnly DateStart
+        {
+            get;
+            set;
+        }
+
+        public DateOnly DateEnd
+        {
+            get;
+            set;
+        }
+
+        public int Id
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
